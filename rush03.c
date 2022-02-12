f7a3e5c6fe8b1c0985f7b1c849784e89cd06a585
veryheavypickle <xavier.carroll01@gmail.com>
2022-02-12 19:40:05 +0100

    TOO MANY LINES

--- rush03.c
+++ rush03.c
@@ -12,24 +12,39 @@
 
 #include <stdio.h>
 
-void ft_putchar(char c);
+void	ft_putchar(char c);
 
-void    rush (int y, int x)
-{   
-    int i = 0; int j = 0; int rows = y - 1; int colummns = x - 1;
-    while (i <= colummns){
-        while (j <= rows) {
-            if ((j == 0  && i == 0) || (j == 0 && i == colummns)) {
-                ft_putchar('A');
-            } else if ((j == rows && i == 0) || (j == rows && i == colummns)) {
-                ft_putchar('C');
-            } else if (i == 0 || j == 0|| i == colummns || j == rows) {
-                ft_putchar('B');
-            } else {
-                ft_putchar(' ');
-            }
-            j++;
-        }
-        j = 0; i++; ft_putchar('\n');
-    }
+void	rush(int y, int x)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i <= x - 1)
+	{
+		while (j <= y - 1)
+		{
+			if ((j == 0 && i == 0) || (j == 0 && i == x - 1))
+			{
+				ft_putchar('A');
+			}
+			else if ((j == y - 1 && i == 0) || (j == y - 1 && i == x - 1))
+			{
+				ft_putchar('C');
+			}
+			else if (i == 0 || j == 0 || i == x - 1 || j == y - 1)
+			{
+				ft_putchar('B');
+			}
+			else
+			{
+				ft_putchar(' ');
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+		ft_putchar('\n');
+	}
 }
