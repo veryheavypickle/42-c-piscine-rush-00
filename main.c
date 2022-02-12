164904f74f585f679b40b9ddb52e3253d6fa449a
mdque-v <mduque-v@student.42madrid.com>
2022-02-12 17:29:23 +0100

    EL BUENO

--- main.c
+++ main.c
@@ -10,10 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-
-int rush (int x, int y);
+void rush (int x, int y);
 
 int main (void) {
 
@@ -25,52 +22,5 @@ int main (void) {
     */
     rush(5,5);
     
-    return (0); 
-}
-
-int rush (int x, int y){
-
-    int i = 0; 
-    int j = 0; 
-    int rows = x; 
-    int colummns = y; 
-
-    char a;
-    char empty;
-    a = 'A';
-    empty = ' ';
-
-    while (i < colummns){
-        i++;
-        j = 0;
-        while (j < rows) {
-            j++;
-            if (j == 1  && i == 1) {
-                write(0, &a, 1);
-            }
-
-            else if (j == 1 && i == y) {
-                write(0, &a, 1);
-            }
-
-            else if (j == x && i == 1) {
-                write(0, &a, 1);
-            }
-            else if (j == x && i == y) {
-                write(0, &a, 1);
-            } else {
-                write(0, &empty, 1);
-            }
-            /*
-            printf("j: %d\ni: %d\n", j, i);
-            printf("x: %d\ny: %d\n\n", x, y);
-            */
-        }
-        /* Write new line */
-        write(0, "\n", 1);
-        
-    }
-
-
     return (0); 
 }
--- rush03.c
+++ rush03.c
@@ -3,87 +3,46 @@
 
 void ft_putchar(char c);
 
-void rush(int x, int y){
+void    rush (int x, int y){
 
-    int i = 0; 
-    int j = 0; 
-    int rows = x - 1; 
-    int colummns = y - 1; 
+    int i = 0;
+    int j = 0;
+    int rows = x;
+    int colummns = y;
 
     char a;
-    char b;
-    char c;
     char empty;
     a = 'A';
-    b = 'B';
-    c = 'C';
     empty = ' ';
-    
 
     while (i < colummns){
-        while (j < rows) {
-            /* Esquinas */
-            /* Fila 1, Columno 1 */
-            if (j == 0  && i == 0) {
-                ft_putchar(a);
-            }
-            /* Fila 1, Columno y */
-            else if (i == 0 && j == rows){
-                ft_putchar(a);
-            }
-            /*
-
-            Fila x, Columno 1
-            else if (j == rows && i == 0) {
-                ft_putchar(c);
-            }
-
-            Fila x, Columno y
-            else if (j == rows && i == colummns) {
-                ft_putchar(c);
-            }
-
-            Fila 1
-            else if (i == 0) {
-                ft_putchar(b);
-            }
-
-            Fila x
-            else if (i == rows) {
-                ft_putchar(b);
-            }
-            
-            Write sides
-            } else if (i == 1) {
-                write(0, &b, 1);
-                ft_putchar(&a);
-            } else if (i == x) {
-                write(0, &b, 1);
-                ft_putchar(&a);
-            } else if (j == 1) {
-                write(0, &b, 1);
-                ft_putchar(&a);
-            } else if (j == y) {
-                write(0, &b, 1);
-                ft_putchar(&a);
-            }
-            */
-
-            /* Write nada */
-            else {
-                ft_putchar(empty);
-            }
-            /*
-
-            printf("J: %d\n", j);
-            printf("I: %d\n", i);
-            */
-            j++;
-        }
-        /* Write new line */\
-        ft_putchar('\n');
         i++;
         j = 0;
+        while (j < rows) {
+            j++;
+            if (j == 1  && i == 1) {
+                write(0, &a, 1);
+            }
+
+            else if (j == 1 && i == y) {
+                write(0, &a, 1);
+            }
+
+            else if (j == x && i == 1) {
+                write(0, &a, 1);
+            }
+            else if (j == x && i == y) {
+                write(0, &a, 1);
+            } else {
+                write(0, &empty, 1);
+            }
+            /*
+            printf("j: %d\ni: %d\n", j, i);
+            printf("x: %d\ny: %d\n\n", x, y);
+            */
+        }
+        /* Write new line */
+        write(0, "\n", 1);
         
     }
 }
