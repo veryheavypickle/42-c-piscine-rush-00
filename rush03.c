bb5e144d8faf9d567dbd4e16d859068f8949827d
mdque-v <mduque-v@student.42madrid.com>
2022-02-12 17:51:42 +0100

    helo

--- rush03.c
+++ rush03.c
@@ -1,42 +1,21 @@
 #include <unistd.h>
 #include <stdio.h>
-
 void ft_putchar(char c);
-
 void    rush (int y, int x){
-
-    int i = 0;
-    int j = 0;
-    int rows = x - 1;
-    int colummns = y - 1;
-
+    int i = 0; int j = 0; int rows = x - 1; int colummns = y - 1;
     while (i <= colummns){
         while (j <= rows) {
-            if (j == 0  && i == 0) {
-                ft_putchar('1');
-            }
-            else if (j == 0 && i == colummns) {
-                ft_putchar('2');
-            }
-
-            else if (j == rows && i == 0) {
-                ft_putchar('3');
-            }
-            else if (j == rows && i == colummns) {
-                ft_putchar('4');
+            if ((j == 0  && i == 0) || (j == 0 && i == colummns)) {
+                ft_putchar('A');
+            } else if ((j == rows && i == 0) || (j == rows && i == colummns)) {
+                ft_putchar('C');
+            } else if (i == 0 || j == 0|| i == rows || j == colummns) {
+                ft_putchar('B');
             } else {
                 ft_putchar(' ');
             }
-            /*
-            printf("j: %d\ni: %d\n", j, i);
-            printf("x: %d\ny: %d\n\n", x, y);
-            */
             j++;
         }
-        j = 0;
-        i++;
-        /* Write new line */
-        ft_putchar('\n');
-        
+        j = 0; i++; ft_putchar('\n');
     }
 }
